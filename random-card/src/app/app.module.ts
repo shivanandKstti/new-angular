81b75bdbc8da3d94d9f340d3ee6b3ec7d194409a
yasharth <yash@simplify3x.com>
2021-04-12 19:08:57 +0530

    showing user detail in card completed

--- random-card/src/app/app.module.ts
+++ random-card/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(),
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ random-card/src/app/card/card.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { faEnvelope, faMapMarkedAlt, faPhone, faDatabase } from '@fortawesome/free-solid-svg-icons';
+
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  @Input() user: any;
+  faEnvelope = faEnvelope;
+  faMapMarkedAlt = faMapMarkedAlt;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
